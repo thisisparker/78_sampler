562e3f4036b5c146c61f8cbdbdc7619e7325fffb
Parker Higgins <parker@parkerhiggins.net>
2020-07-20 18:26:39 -0400

    improve metadata handling for cleaner status text

--- 78special.py
+++ 78special.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
-import random
-from georgeblood import blood
 import internetarchive as ia
-from operator import attrgetter
 import os
+import random
 import subprocess
-from mutagen.mp3 import MP3
-from twython import Twython
 import yaml
+
+from operator import attrgetter
+
+from mutagen.mp3 import MP3
+from PIL import Image
+from twython import Twython
+
+from georgeblood import blood
 
 def main():
     abspath = os.path.abspath(__file__)
@@ -16,9 +20,10 @@ def main():
     os.chdir(dname)
 
     tune = random.choice(blood)
-    item = ia.get_files(tune['identifier'], formats=['VBR MP3', 'Item Image'])
+    item = ia.get_item(tune['identifier'])
+    files = item.get_files(formats=['VBR MP3', 'Item Image'])
 
-    l = list(item)
+    l = list(files)
 
     mp3s = [f for f in l if f.format == 'VBR MP3']
     photos = [f for f in l if f.format == 'Item Image']
@@ -26,9 +31,12 @@ def main():
     photo = max(photos, key=attrgetter('size'))
     photo.download(photo.name)
 
-    to_dl = min(mp3s, key=attrgetter('name'))
+    to_dl = min(mp3s, key=lambda s: len(s.name))
 
-    title = to_dl.name.split('.mp3')[0]
+    title = item.metadata.get('title')
+    artists = item.metadata.get('creator')
+    artists = ', '.join(artists) if type(artists) is list else artists
+
     url = "https://archive.org/details/" + to_dl.identifier
 
     print("downloading", title)
@@ -61,7 +69,7 @@ def main():
     print(title.lower(), url, "merge.mp4", sep=" ")
 
     with open('config.yaml') as f:
-        config = yaml.load(f)
+        config = yaml.safe_load(f)
 
     twitter_app_key = config['twitter_app_key']
     twitter_app_secret = config['twitter_app_secret']
@@ -73,7 +81,8 @@ def main():
     with open('merge.mp4', 'rb') as f:
         response = twitter.upload_video(media=f, media_type='video/mp4', media_category='tweet_video', check_progress = True)
 
-    status = " ".join([title.lower(), url])
+    status = " ".join([title.lower() + ' - ' + artists.lower(), url])
+    print(status)
     twitter.update_status(status=status, media_ids=[response['media_id']])
 
 if __name__ == '__main__':
